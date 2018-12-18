0f3b9384e286bc7e3d80cf31a5ef93933a59ca0f
ZLJULIA <2804251031@qq.com>
2018-12-18 17:24:27 +0800

    Update App.java

--- src/com/znl/zl/spider/App.java
+++ src/com/znl/zl/spider/App.java
@@ -1,6 +1,8 @@
 package com.znl.zl.spider;
 
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,8 +13,8 @@ import java.util.concurrent.Executors;
  */
 
 public class App {
-	
-   //main+alt+/
+
+	// main+alt+/
 
 	public static void main(String[] args) {
 		
@@ -25,13 +27,48 @@ public class App {
 //		 pool=Executors.newSingleThreadExecutor();
 	     ArrayList<Film> list=new ArrayList();
 	     String url="https://movie.douban.com/top250?start";
-		 for(int i=1;i<=100;i++){
+		 for(int i=0;i<10;i++){
 			 String path=String.format("%s=%d", url,i*25);
 			 pool.submit(new Spider(path,list)); 
 		 }
-		
-		
+	
+		 pool.shutdown();
+		 System.out.println(pool.isTerminated());
 
+		while(!pool.isTerminated()){
+			//休眠，每隔一秒钟检测是否结束
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		//数据排序
+		System.out.println(list.size());
+//		for(Film film:list)
+//		{
+//			System.out.print(film.toCSV());
+//		}
+		
+		//写入文件
+		String file="d:/film.csv";//绝对路径
+		file="film.csv";//相对路径
+		
+		//排序
+		Collections.sort(list);
+		//io,try+alt+/
+		
+		try {
+			FileWriter out = new FileWriter(file);
+//			写数据
+			for (Film film : list) {
+				out.write(film.toCSV());
+			}
+			System.out.println("ok");
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
