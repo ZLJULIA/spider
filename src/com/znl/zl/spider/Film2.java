328ecadaddb4a5e351570dec02f2a9208b6aa33f
ZLJULIA <2804251031@qq.com>
2018-12-18 08:02:55 +0800

    Create Film2.java

---
+++ src/com/znl/zl/spider/Film2.java
@@ -0,0 +1,46 @@
+package com.znl.zl.spider;
+
+public class Film2 {
+	/*
+	 * 导演
+	 */
+      String director;
+      /*
+       * 编剧
+      String actor;
+       */
+     String screenwriter;
+     /*
+      * 主演
+      */
+	 /*
+	  * 类型
+	  */
+	 String type;
+	 /*
+	  * 制片国家
+	  */
+	 String produceregion;
+	 /*
+	  * 上映时间
+	  */
+	 String showtime;
+	 /*
+	  * 片长
+	  */
+	 int length;
+	 /*
+	  * 又名
+	  */
+	 String second;
+	@Override
+	public String toString() {
+		return "Film2 [director=" + director + ", screenwriter=" + screenwriter
+				+ ", type=" + type + ", produceregion=" + produceregion
+				+ ", showtime=" + showtime + ", length=" + length + ", second="
+				+ second + "]";
+	}
+}
+	 
+	 
+	 
