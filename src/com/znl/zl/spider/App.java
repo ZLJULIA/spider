f946297ff608f527f33cddf19a60a19184c91c19
ZLJULIA <2804251031@qq.com>
2018-12-18 11:34:07 +0800

    Update App.java

--- src/com/znl/zl/spider/App.java
+++ src/com/znl/zl/spider/App.java
@@ -1,12 +1,8 @@
 package com.znl.zl.spider;
 
-import java.io.IOException;
 import java.util.ArrayList;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /*
  * 程序的入口
@@ -19,46 +15,23 @@ public class App {
    //main+alt+/
 
 	public static void main(String[] args) {
-	//设置需要抓取的路径：目标URL	
-		String url="https://movie.douban.com/top250";
-	//使用JSOUP 抓取文档
-		try {
-			Document doc=Jsoup.connect(url).get();
-			Elements es=doc.select(".grid_view .item");
-			System.out.println(es.size());
-			
-			//创建一个影片的列表
-			ArrayList<Film> list=new ArrayList();
-			
-			for(Element e :es){
-				Film f=new Film();
-				
-				//每一部影片的信息
-				
-				f.id=Integer.parseInt(e.select(".pic em").first().text());
-				f.poster=e.select("img").first().attr("src");
-				f.info=e.select(".bd p").first().text();
-				f.title=e.select(".title").first().text();
-				f.rating=Double.parseDouble(e.select(".rating_num").first().text());
-				String num=e.select(".star span").last().text();
-				f.num=Integer.parseInt(num.substring(0,num.length()-3));
-				f.quote=e.select(".inq").first().text();
-//				
-				list.add(f);
-				System.out.println(f);
-			}
-//			String title=doc.title();
-//			String data=doc.data();
-			
-			//syso+alt+/
-//			System.out.println(title);
-//			System.out.println(data);
-			
-		    
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
+		  //线程池
+		  //固定大小
+		 ExecutorService pool=Executors.newFixedThreadPool(4);
+		 //无线（缓存）
+		 pool=Executors.newCachedThreadPool();
+//		 //一个线程
+//		 pool=Executors.newSingleThreadExecutor();
+	     ArrayList<Film> list=new ArrayList();
+	     String url="https://movie.douban.com/top250?start";
+		 for(int i=1;i<=100;i++){
+			 String path=String.format("%s=%d", url,i*25);
+			 pool.submit(new Spider(path,list)); 
+		 }
+		
+		
+
 	}
 
 }
